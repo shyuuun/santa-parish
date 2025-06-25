e93ecfbe325eecd63a99349e9f7c14b751d950be
shyuuun <m4rshm4ll0w14@gmail.com>
2025-06-26 07:45:53 +0800

    feat: define route types and structure for authentication and admin

--- src/utils/route.ts
+++ src/utils/route.ts
@@ -1,1 +1,18 @@
 // Routes for the application
+
+type Route = Record<string, string>;
+
+export const auth: Route = {
+	login: "/login",
+	logout: "/logout",
+	register: "/register",
+	forgetPassword: "/forget-password",
+};
+
+export const user: Route = {};
+
+export const admin: Route = {
+	dashboard: "/d"
+};
+
+

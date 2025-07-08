6aa87fe32817fe925ba8cf0246b6a41b5fad4608
shyuuun <m4rshm4ll0w14@gmail.com>
2025-07-09 07:42:10 +0800

    added logout functionallity in dashboard

---
+++ src/app/dashboard/(overview)/actions/logout.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { createClient } from "@/src/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export async function logout() {
+	const supabase = await createClient();
+	
+	const { error } = await supabase.auth.signOut();
+	
+	if (error) {
+		console.error("Error signing out:", error);
+		throw new Error("Failed to sign out");
+	}
+	
+	redirect("/login");
+}
